78e13c0795ed4f32948408c8791a8b2db34cd94a
iMac11 <fool@iMac11.local>
2021-08-31 22:44:20 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FiddlerCore.Trial._5._0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
